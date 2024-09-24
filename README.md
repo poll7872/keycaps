eb928a5c5397acf97d561ef28b51973bb4cf38d8
Edward Ojeda <edward.ojeda.es@gmail.com>
2024-09-23 20:45:49 -0500

    Proyecto de: Edward Rolando Ojeda España

---
+++ README.md
@@ -0,0 +1,4 @@
+# Proyecto Final Introducción lenguajes de programación.
+
+- A continuación se muestra el desarrollo de un sitio web para una empresa que desea vender teclados mecánicos. 
+- Para el desarrollo de este sitio web se utilizo: HTML, CSS.
---
+++ js/menu.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuIcon = document.getElementById("menu-icon")
+    const navBar = document.querySelector(".nav-bar")
+
+    menuIcon.addEventListener('click', () => {
+        navBar.classList.toggle('active')
+    })
+})
